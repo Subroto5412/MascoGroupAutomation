41c3963cf5880c9cb7642043edbc34c08230a1d7
Subroto <subroto_mohonto>
2021-08-19 14:03:20 +0600

    remove test

--- app/src/main/java/com/bd/mascogroup/automation/ui/home/HomeActivity.kt
+++ app/src/main/java/com/bd/mascogroup/automation/ui/home/HomeActivity.kt
@@ -22,9 +22,7 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>(), IHomeNa
 
     @Inject
     lateinit var mHomeViewModel: HomeViewModel
-
-//Hello Subroto
-
+    
     override val bindingVariable: Int
         get() = BR.viewModel
 
